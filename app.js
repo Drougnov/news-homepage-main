cce294455c5898e629fb758a4b4bc3da690b0aad
RockyBaura <251drougnov@gmail.com>
2022-10-25 10:37:55 +0600

    Nav close on link clicked, add class on container

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const toggleMenu = document.querySelector('.nav__menu');
+const container = document.querySelector('.container');
 const nav = document.querySelector('nav');
+const toggleMenu = document.querySelector('.nav__menu');
+const navLinks = document.querySelectorAll('.nav__list li a')
 
 toggleMenu.addEventListener('click',()=>{
     nav.classList.toggle('active');
+    container.classList.toggle('active');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
 })
