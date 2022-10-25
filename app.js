10e813f8698021df6982b18b29c5f52d91010293
RockyBaura <251drougnov@gmail.com>
2022-10-25 11:11:40 +0600

    Disable scrolling when nav active

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const navLinks = document.querySelectorAll('.nav__list li a')
 toggleMenu.addEventListener('click',()=>{
     nav.classList.toggle('active');
     container.classList.toggle('active');
+    if(nav.classList.contains('active')){
+        document.body.style.overflow = "hidden";
+    }else{
+        document.body.style.overflow = "visible";
+    }
 })
 
 navLinks.forEach(link => {
     link.addEventListener('click',()=>{
         nav.classList.remove('active');
+        container.classList.remove('active');
+        document.body.style.overflow = "visible";
     })
 })
